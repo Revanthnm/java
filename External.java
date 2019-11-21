c273428492219ed0a8821962b5b9e72a0ac75f05
Revanth.NM <53811741+Revanthnm@users.noreply.github.com>
2019-11-22 00:32:53 +0530

    Add files via upload

---
+++ External.java
@@ -0,0 +1,14 @@
+package See;
+
+
+public class External{
+	
+	public int seemarks[];
+	
+	public External(){
+	
+		seemarks = new int[5];
+	
+	}
+
+}
---
+++ Internals.java
@@ -0,0 +1,19 @@
+package Cie;
+
+
+
+import java.util.*;
+
+
+public class Internals{
+	
+	public int marks[];
+
+
+	public Internals(){
+		
+		marks = new int[5];
+	
+	}
+
+}
